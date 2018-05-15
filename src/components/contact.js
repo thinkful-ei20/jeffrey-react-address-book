11a5645195b035de73d3d76e4a55b6bf6be0c360
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-05-15 15:29:54 -0500

    AddressBook and Contact structure

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,3 +3,12 @@ import React from 'react';
 import './contact.css';
 
 // The Contact component goes here.  It should be the default export.
+export default function Contact(props) {
+  return (
+    <section>
+      <img src={props.photo} alt={props.name}/>
+      <h2>{props.name}</h2>
+      <address>{props.address}</address>
+    </section>
+  );
+}
