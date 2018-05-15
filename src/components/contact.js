e33ad053b2fc320fc76f37ed6a87f327da6cb81f
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-05-15 15:32:50 -0500

    Contact style

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,10 +5,10 @@ import './contact.css';
 // The Contact component goes here.  It should be the default export.
 export default function Contact(props) {
   return (
-    <section>
-      <img src={props.photo} alt={props.name}/>
-      <h2>{props.name}</h2>
-      <address>{props.address}</address>
+    <section className="contact">
+      <img className="contact-photo" src={props.photo} alt={props.name}/>
+      <h2 className="contact-name">{props.name}</h2>
+      <address className="contact-address">{props.address}</address>
     </section>
   );
 }
